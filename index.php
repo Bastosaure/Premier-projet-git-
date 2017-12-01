0a30f6a367a8a06c4f80c77c483e524db2e5560e
Mat <lecoeur.matthieu@gmail.com>
2017-12-01 15:04:06 +0100

    copains

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include('inc/pdo.php'); ?>
+
+
+
+
+
+
+
+
+<?php include('inc/header.php'); ?>
+
+
+
+<h1> Salut les copains product'Eure</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include('inc/footer.php');?>
